f4ba64f1963a6b478524f71fd6146517695f1a01
HamsterofDeath <d.haupt82@gmail.com>
2017-09-20 23:58:39 +0200

    ...

--- src/main/scala/hod/Analyzer.scala
+++ src/main/scala/hod/Analyzer.scala
@@ -689,13 +689,13 @@ object Analyzer extends spells.Spells {
         0.0 -> "etc",
         35.0 -> "lsk",
         0.0 -> "ltc",
-        894.0 -> "nav",
+        144.0 -> "nav",
         0.0 -> "nlg",
         0.0 -> "pivx",
-        0.0 -> "rads",
-        0.0 -> "strat",
+        49.0 -> "rads",
+        48.0 -> "strat",
         0.0 -> "usdt",
-        0.0 -> "waves",
+        58.0 -> "waves",
         0.0 -> "xmr",
         0.0 -> "xrp"
       )
