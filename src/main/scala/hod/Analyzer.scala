fefb651f9e28c188002af6cda195e86b250a8925
hamsterofdeath <h-star@gmx.de>
2017-10-06 11:59:12 +0200

    ...

--- src/main/scala/hod/Analyzer.scala
+++ src/main/scala/hod/Analyzer.scala
@@ -680,7 +680,7 @@ object Analyzer extends spells.Spells {
 
     val gottenViaTransactions = {
       buildBalance(
-        0.0 -> "bcc",
+        0.51 -> "bcc",
         0.0 -> "btc",
         0.0 -> "dash",
         0.0 -> "doge",
@@ -689,11 +689,11 @@ object Analyzer extends spells.Spells {
         0.0 -> "etc",
         0.0 -> "lsk",
         0.0 -> "ltc",
-        459.0 -> "nav",
-        0.0 -> "nlg",
+        0.0 -> "nav",
+        3198.0 -> "nlg",
         0.0 -> "pivx",
         0.0 -> "rads",
-        163.0 -> "strat",
+        113.0 -> "strat",
         0.0 -> "usdt",
         0.0 -> "waves",
         0.0 -> "xmr",
